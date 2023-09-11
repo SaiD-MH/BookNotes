3c6223bb8da8c5cc4d7a1048ce09ca94f22b22f9
mohamed Saeed <muhamdsaid253@gamil.com>
2023-09-11 12:13:57 +0300

    Ch - 15 in Head First Java 3rd Edition

--- Java/Head First Java/CH - 15/test/Server.java
+++ Java/Head First Java/CH - 15/test/Server.java
@@ -1,32 +1,64 @@
 import java.io.*;
 import java.net.*;
-public class Server {
- String[] adviceList = {"1" , "2" , "3"};
- public void go() {
- try {
- ServerSocket serverSock = new ServerSocket(4242);
- while(true) {
-    System.out.println("Waiiiiiiiiiit");
- Socket sock = serverSock.accept();
- 
- PrintWriter writer = new PrintWriter(sock.getOutputStream());
- String advice = getAdvice();
- writer.println(advice);    
- writer.close(); 
- System.out.println(advice); 
- }
- } catch(IOException ex) {
- ex.printStackTrace();
- }
- } // close go
+import java.util.*;
 
- private String getAdvice() {
- int random = (int) (Math.random() * adviceList.length);
- return adviceList[random];
- }
- 
- public static void main(String[] args) {
- Server server = new Server();
- server.go();
- }
+
+public class Server
+{
+    ArrayList clientOutputStreams;
+    
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+        
+        public ClientHandler(Socket clientSOcket) {
+            try {
+                sock = clientSOcket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+                
+            } catch (Exception ex) { ex.printStackTrace(); }
+        }
+        
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+            } catch (Exception ex) { ex.printStackTrace(); }
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Server().go();
+    }
+    
+    public void go() {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSock = new ServerSocket(5000);
+            while(true) {
+                Socket clientSocket = serverSock.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+                
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                System.out.println("got a connection");
+            }
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+    
+    public void tellEveryone(String message) {
+        Iterator it = clientOutputStreams.iterator();
+        while (it.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.println(message);
+                writer.flush();
+            } catch (Exception ex) { ex.printStackTrace(); }
+        }
+    }
 }
