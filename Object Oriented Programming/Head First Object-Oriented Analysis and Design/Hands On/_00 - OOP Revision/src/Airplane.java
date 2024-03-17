cc92baf08fac9b477de76b7efed8f1f88fd391eb
SaiD-MH <muhamdsaid253@gmail.com>
2024-03-17 02:06:36 +0200

    00 - OOP Revision

---
+++ Object Oriented Programming/Head First Object-Oriented Analysis and Design/Hands On/_00 - OOP Revision/src/Airplane.java
@@ -0,0 +1,12 @@
+public class Airplane {
+
+    public int speed;
+    public Airplane() {
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+}
